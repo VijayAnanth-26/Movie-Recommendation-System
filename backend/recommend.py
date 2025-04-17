d60e891a77c1bd03e178c148de250562eda7e193
VijayAnanth-26 <vijayananth09876@gmail.com>
2025-04-17 22:27:32 +0530

    new

--- backend/recommend.py
+++ backend/recommend.py
@@ -54,7 +54,7 @@ movies['overview'] = movies['overview'].apply(lambda x: x.split())
 movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
 new_df = movies[['movie_id', 'title', 'tags']]
 new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x))
-new_df['tags'] = new_df['tags'].apply(lambda x: x.lower())
+new_df['tags'] = new_df['tags'].str.lower()
 
 # Vectorization
 cv = CountVectorizer(max_features=5000, stop_words='english')
